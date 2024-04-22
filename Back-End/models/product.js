19316419236589c1c6f69c4d31a5e4524fa098f2
ismail939 <ismailsalama526@gmail.com>
2024-04-22 16:17:05 +0200

    added cart and product models

---
+++ Back-End/models/product.js
@@ -0,0 +1,22 @@
+module.exports = (db, type) => {
+    return db.define('products', {
+        id:{
+            type: type.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name:{
+            type:type.STRING,
+            allowNull:false
+        },
+        quantity_available:{
+            type: type.INTEGER, 
+            allowNull: false
+        },
+        description:{
+            type: type.TEXT,
+            allowNull: false
+        }
+    })
+
+}
